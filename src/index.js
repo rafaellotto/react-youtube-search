80dcd20539bc71e8e708dc414a4edd29baad1c8e
ralomach <ralomach@gmail.com>
2018-10-01 14:39:42 -0300

    add custom YTSearch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import YTSearch from './components/YTSearch';
 import SearchBar from './components/searchBar';
 import VideoDetail from './components/videoDetail';
 import VideoList from './components/videoList';
